b54cfcb52c68cb0f6c380cfa594e1635416d85a7
iteye <iteyelmp@gmail.com>
2026-04-21 17:11:31 +0800

    update package

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   configureWebpack:{
     optimization: {
@@ -5,7 +7,23 @@ module.exports = {
         minSize: 1024000,
         maxSize: 1024000,
       }
-    }
+    },
+    resolve: {
+      fallback: {
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "assert": require.resolve("assert/"),
+        "buffer": require.resolve("buffer/"),
+        "process": require.resolve("process/browser"),
+        "vm": require.resolve("vm-browserify")
+      }
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer']
+      })
+    ]
   },
   productionSourceMap: false,
   publicPath: '/',
